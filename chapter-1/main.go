56350e29facef73477141582ed79acad7f334182
Shubhangcs <cshubhanga@gmail.com>
2024-06-15 11:52:11 +0530

    no issue

---
+++ chapter-1/main.go
@@ -0,0 +1,12 @@
+// this package tells the go compiler that from where to start the execution from
+package main
+
+//importing required package or standard libraries are done this way
+import "fmt"
+
+// this is a function execution part of a program the name of function is main which indicates that this function is entry point of
+//the program
+func main(){
+	//Println is a standart function in fmt package to print output to console
+	fmt.Println("Hello World")
+}
---
+++ chapter-2/main.go
@@ -0,0 +1,33 @@
+package main
+
+// In this program we will discuss about basic data types in go
+
+import "fmt"
+
+func main(){
+	//there are many types in go they are
+	/*
+	->int
+		-int8
+		-int16
+		-int32
+		-int64
+		-int (default 32bit)
+	->uint
+		-uint8
+		-uint16
+		-uint32
+		-uint64
+		-uint (default 32bit)
+	->float
+		-float32
+		-float64
+	->bool
+		-imp thing to note that bool cannot be converted to integer they are different in go.
+	->complex
+		-complex
+		-complex-64
+		-complex128
+	*/
+	fmt.Println("Variables")
+}
